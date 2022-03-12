770c918bd6e7e4e66b82fd2566ebaafcbe0f88e6
ArshiaP <arshiaputhran08@gmail.com>
2022-03-12 09:21:57 +0530

    Login

--- riptide/src/pages/login/login.js
+++ riptide/src/pages/login/login.js
@@ -8,9 +8,9 @@ function Login() {
         <div>
             <PrettyPartciles/>
             <h1 className='login-heading'>Welcome back!</h1>
-            <p className='login-para'>Not a member?<Link to='/register'>Sign up.</Link> </p>
-            <input type="email" className="email-login-input" placeholder='email'/>
-            <input type="password" className="password-login-input" placeholder='password'/>
+            <p className='login-para'>Not a member? <Link to='/register' className='login-signUp'>Sign up.</Link> </p>
+            <input type="email" className="email-login-input" placeholder='Email'/>
+            <input type="password" className="password-login-input" placeholder='Password'/>
             <button type="submit" className="btn-input" value="Submit">Log in</button>
 
         </div>
